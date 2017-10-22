da23c27b0384dc63d076ea5123d505f8be0d4bb4
DeeplyEmbedded <deeplyembeddedWP@users.noreply.github.com>
2017-10-22 11:28:01 +0530

    Bug Fix

--- SPI_Library/SPI.h
+++ SPI_Library/SPI.h
@@ -34,8 +34,8 @@ SOFTWARE.
 #define SPIDEV_BYTES_NUM                 8
 #define SPIDEV_DATA_BITS_NUM             8
 #define SPIDEV_DELAY_US	                 0
-#define SPI_CS_HIGH                      1
-#define SPI_CS_LOW                       0
+#define SPI_SS_HIGH                      1
+#define SPI_SS_LOW                       0
 #define SPI_ONE_BYTE                     1
 
 /* No. of bytes per transaction */
@@ -72,14 +72,15 @@ extern int Set_SPI_mode(int fd, unsigned char spi_mode);
 extern int Set_SPI_bits(int fd, unsigned char bits_per_word);
 extern int Set_SPI_speed(int fd, unsigned long bus_speed_HZ);
 extern void SPI_Config_init(unsigned long spi_bytes_no, unsigned long spi_bus_speed,
-		unsigned char chip_select, unsigned short spi_delay,
-		unsigned char spi_bits_No, unsigned char mode_spi, SPI_DevicePtr SPI_devicePtr);
+                unsigned char chip_select, unsigned short spi_delay,
+                unsigned char spi_bits_No, unsigned char mode_spi, SPI_DevicePtr SPI_devicePtr);
 
-/* API's to initialize and use spidev1.0 - Pre-Configured */
-extern int SPI_DEV1_init();
+/* API's to initialize and use spidev1.0 - Configure the Bus as you like*/
+extern int SPI_DEV1_init(unsigned long spi_bytes_no, unsigned long spi_bus_speed,
+                                           unsigned char chip_select, unsigned short spi_delay,
+                                           unsigned char spi_bits_No, unsigned char mode_spi);
 extern int SPIDEV1_transfer(unsigned char *send, unsigned char *receive,
-		unsigned char bytes_num);
+                unsigned char bytes_num);
 extern unsigned char SPIDEV1_single_transfer(unsigned char data_byte);
-
 
 #endif /* SPI_H_ */
